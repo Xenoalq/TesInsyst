c2b8b9a4384a1a893e8c2c2d74355a33b10ad3b8
Xenoalq <skynikeshd11@gmail.com>
2025-11-20 11:00:15 +0700

    chore: move product view and create layout master

--- public/js/product.js
+++ public/js/product.js
@@ -1,14 +1,11 @@
 $(document).ready(function () {
 
-    // Init datatable
     $('#productTable').DataTable();
 
-    // Reset modal saat klik tambah
     $('#btnAddProduct').click(function () {
         $('#productForm')[0].reset();
     });
 
-    // Submit form
     $('#productForm').on('submit', function (e) {
         e.preventDefault();
 
